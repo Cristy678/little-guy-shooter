31923b0db6ca4322d1653dd4fdae2b8867bfc573
Cristy678 <cris.oyarzen@gmail.com>
2026-02-07 20:39:48 -0300

    Indev.A.9.9.9
    Added HP Variable, Combo and HP Number Display

--- scripts/CharacterMovement.gd
+++ scripts/CharacterMovement.gd
@@ -2,17 +2,22 @@ class_name Player
 extends CharacterBody2D
 
 var bullet_scene = preload("res://scenes/bullet.tscn")
+@export var hp = 100
 @export var ammo = 100
 @export var loadedammo = 0
 @export var speed = 320
 @onready var animated_sprite: AnimatedSprite2D = $Player
 @onready var ammoHUD: Label = get_parent().get_node("UIscreen/AmmoHUD")
 @onready var loadammoHUD: Label = get_parent().get_node("UIscreen/LoadAmmoHUD")
+@onready var hpHUD: Label = get_parent().get_node("UIscreen/HpHUD")
+@onready var comboHUD: Label = get_parent().get_node("UIscreen/ComboHUD")
 @onready var gun: Sprite2D = $Player/Gun01
 
 func _ready() -> void:
 	ammoHUD.text = str(ammo)
 	loadammoHUD.text = str(loadedammo)
+	hpHUD.text = str(hp)
+	comboHUD.text = str(0)
 	
 func _process(_delta: float) -> void:
 	var input_vector = Input.get_vector("move_left","move_right","move_up","move_down")
@@ -41,7 +46,7 @@ func _process(_delta: float) -> void:
 		if loadedammo > 0:
 			bullet.global_position = gun.global_position
 			bullet.direction = (get_global_mouse_position() - global_position).normalized()
-			$/root/playground.add_child(bullet)
+			get_parent().add_child(bullet)
 			loadedammo = loadedammo - 1
 			ammoHUD.text = str(ammo)
 			loadammoHUD.text = str(loadedammo)
